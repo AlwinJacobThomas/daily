85f46061025fdf1065073bce9f9641f87cdfbf17
Alwin J Thomas <33421258+AlwinJacobThomas@users.noreply.github.com>
2022-08-25 19:42:03 +0530

    Edited names of urls

--- dailytourneys/urls.py
+++ dailytourneys/urls.py
@@ -4,9 +4,9 @@ from django.urls import path,include
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('signup', views.signup),
-    path('signin', views.signin),
-    path('signout', views.signout),
-    path('postanad', views.postanad),
+    path('signup', views.signup,name='signup'),
+    path('signin', views.signin,name='signin'),
+    path('signout', views.signout,name='signout'),
+    path('postanad', views.postanad,name='postanad'),
     #path('details/<id:pk>',views.details,  name= 'details')
 ]
